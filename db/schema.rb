c1ead9fffaf4796f8672819951465dbff7fb0d0d
Nicolas Radu <nicolasradu16@gmail.com>
2013-03-21 11:56:47 +0100

    Online Exercises and Qcms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320221634) do
+ActiveRecord::Schema.define(:version => 20130321094454) do
 
   create_table "chapters", :force => true do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(:version => 20130320221634) do
     t.integer  "position"
     t.datetime "created_at",                    :null => false
     t.datetime "updated_at",                    :null => false
+    t.boolean  "online",     :default => false
   end
 
   create_table "prerequisites", :force => true do |t|
@@ -67,8 +68,9 @@ ActiveRecord::Schema.define(:version => 20130320221634) do
     t.boolean  "many_answers"
     t.integer  "chapter_id"
     t.integer  "position"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+    t.boolean  "online",       :default => false
   end
 
   create_table "sections", :force => true do |t|
