5f256edb7f727d10cff3feb9f31974e11392d53d
Benoît Legat <benoit.legat@gmail.com>
2013-04-09 11:41:09 +0200

    Validates that tostudents is a boolean in the model

--- app/models/actuality.rb
+++ app/models/actuality.rb
@@ -2,4 +2,5 @@ class Actuality < ActiveRecord::Base
   attr_accessible :content, :title, :tostudents
   validates :title, presence: true, length: { maximum: 255 }
   validates :content, presence: true
+  validates :tostudents, inclusion: { in: [false, true] }
 end
