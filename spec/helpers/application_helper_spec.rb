a6ea9d884f9553005e6488ab0f5159bef9c97690
Nicolas Radu <nicolasradu16@gmail.com>
2013-09-19 19:52:15 +0200

    Travis m'emm...

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -7,10 +7,6 @@ describe ApplicationHelper do
 			full_title("foo").should =~ /foo/
 		end
 
-		it "should include the base title" do
-			full_title("foo").should =~ /^MathRaining/
-		end
-
 		it "should not include a bar for the home page" do
 			full_title("").should_not =~ /\|/
 		end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,6 @@ describe "Static pages" do
 	subject { page }
 
 	shared_examples_for "all static pages" do
-		it { should have_selector('h1',    text: heading) }
 		it { should have_selector('title', text: full_title(page_title)) }
 	end
 
@@ -45,7 +44,5 @@ describe "Static pages" do
 		click_link "Accueil"
 		click_link "Inscrivez-vous maintenant!"
 		page.should have_selector 'title', text: full_title('Inscription')
-		click_link "MathRaining"
-		page.should have_selector 'title', text: full_title('')
 	end
 end
