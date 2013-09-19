2eadf2cdccbbd0e0fad6497c8bbba7101209ae60
Nicolas Radu <nicolasradu16@gmail.com>
2013-09-19 17:15:25 +0200

    R minuscule

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:session][:email])
     if user && user.authenticate(params[:session][:password])
       if user.email_confirm
-        flash[:success] = 'Bienvenue sur MathRaining!'
+        flash[:success] = 'Bienvenue sur Mathraining!'
         sign_in user
         redirect_back_or root_path
       else
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   # Returns the full title on a per-page basis.
   def full_title(page_title)
-    base_title = "MathRaining"
+    base_title = "Mathraining"
     if page_title.empty?
       base_title
     else
