2dd9f01424c92b0b27ee5489b42585efd3d4733e
Nicolas Radu <nicolasradu16@gmail.com>
2013-10-06 23:29:21 +0200

    Oups

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,9 +13,11 @@ class UserMailer < AsyncMailer
     mail(to: @user.email, subject: "Mathraining - Mot de passe oublié", from: "mathraining@mathraining.be")
   end
   
-  def new_followed_message(userid, subjectid)
+  def new_followed_message(userid, subjectid, qui, message)
     @user = User.find(userid)
     @subject = Subject.find(subjectid)
+    @qui = qui
+    @message = message
     @tot = @subject.messages.count
     @page = [0,((@tot-1)/10).floor].max + 1
     mail(to: @user.email, subject: "Mathraining - Nouveau message sur le sujet '" + @subject.title + "'", from: "mathraining@mathraining.be")
