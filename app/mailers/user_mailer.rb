b93d27f4bc0f3e02aa1f0ace5611108abab7e381
Nicolas Radu <nicolasradu16@gmail.com>
2013-10-06 23:24:56 +0200

    Seem to be debugged

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,21 +5,19 @@ class UserMailer < AsyncMailer
   
   def registration_confirmation(userid)
     @user = User.find(userid)
-    mail(to: @user.email, subject: "Mathraining - Confirmation d'inscription", from: "no-reply@mathraining.be")
+    mail(to: @user.email, subject: "Mathraining - Confirmation d'inscription", from: "mathraining@mathraining.be")
   end
   
   def forgot_password(userid)
     @user = User.find(userid)
-    mail(to: @user.email, subject: "Mathraining - Mot de passe oublié", from: "no-reply@mathraining.be")
+    mail(to: @user.email, subject: "Mathraining - Mot de passe oublié", from: "mathraining@mathraining.be")
   end
   
-  def new_followed_message(userid, subjectid, qui, message)
+  def new_followed_message(userid, subjectid)
     @user = User.find(userid)
     @subject = Subject.find(subjectid)
-    @qui = qui
-    @message = message
     @tot = @subject.messages.count
     @page = [0,((@tot-1)/10).floor].max + 1
-    mail(to: @user.email, subject: "Mathraining - Nouveau message sur le sujet '" + @subject.title + "'", from: "no-reply@mathraining.be")
+    mail(to: @user.email, subject: "Mathraining - Nouveau message sur le sujet '" + @subject.title + "'", from: "mathraining@mathraining.be")
   end
 end
