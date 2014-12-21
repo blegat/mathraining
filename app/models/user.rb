d66c402e7e42e8e63c7773838d1819a9fe8774cf
Nicolas Radu <nicolasradu16@gmail.com>
2014-12-21 22:14:02 +0100

    Modify user without password

--- app/models/user.rb
+++ app/models/user.rb
@@ -50,10 +50,11 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 	  uniqueness: { case_sensitive: false }
-  validates :password, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }, on: :create
+  validates :password, length: { minimum: 6 }, on: :update, allow_blank: true
   # presence: true <- commented to avoid
   # duplication of error message with confirmation
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, on: :create
 
   def name
     if self.seename == 0
