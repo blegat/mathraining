3d6c0a95f344fdec9d394d986ce400b6354ff61c
Nicolas Radu <nicolasradu16@gmail.com>
2015-03-18 20:31:12 +0100

    Change a little

--- db/migrate/20150318184535_add_lastconnexion_to_users.rb
+++ db/migrate/20150318184535_add_lastconnexion_to_users.rb
@@ -1,5 +1,5 @@
 class AddLastconnexionToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :last_connexion, :date
+    add_column :users, :last_connexion, :date, :default => '2009-01-01'
   end
 end
