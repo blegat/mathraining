5c80ba7114f8015bef9cad59e7e615db738c6051
Nicolas Radu <nicolasradu16@gmail.com>
2016-10-14 23:32:22 +0200

    Details for groups

--- app/models/user.rb
+++ app/models/user.rb
@@ -208,12 +208,13 @@ class User < ActiveRecord::Base
   	end
   end
   
-  def linked_name
+  def linked_name(fullname = false)
   	if !self.corrector?
   		return "<a href='#{Rails.application.routes.url_helpers.user_path(self)}' style='color:#{self.level[:color]};'><span style='color:#{self.level[:color]}; font-weight:bold;'>#{html_escape(self.name)}</span></a>"
   	else
   		debut = self.name[0]
-			fin = self.name[1..-1]
+			fin = self.name[1..-1] unless fullname
+			fin = self.fullname[1..-1] if fullname
   		return "<a href='#{Rails.application.routes.url_helpers.user_path(self)}' style='color:#{self.level[:color]};'><span style='color:black; font-weight:bold;'>#{debut}</span><span style='color:#{self.level[:color]}; font-weight:bold;'>#{html_escape(fin)}</span></a>" 
   	end
   end
