8f60635769eec1f04cad85f10c6d3701eab1fdd3
Nicolas <nicolasradu16@gmail.com>
2021-09-06 23:01:52 +0200

    Cannot have a default for a reference column

--- db/migrate/20140714072407_many_sections_to_one_section_bis.rb
+++ db/migrate/20140714072407_many_sections_to_one_section_bis.rb
@@ -1,5 +1,5 @@
 class ManySectionsToOneSectionBis < ActiveRecord::Migration[5.0]
   def change
-    add_column :chapters, :section, :references, :default => 7
+    add_column :chapters, :section, :references
   end
 end
