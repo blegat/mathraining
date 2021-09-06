9b7d3347caf9af03bc81d096bebc1f2f8ecf2c36
Nicolas <nicolasradu16@gmail.com>
2021-09-06 15:03:07 +0200

    Move email credentials for rails 5.2.0

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address				=> "smtp.gmail.com",
   :port					=> 587,
   :domain				=> "mathraining",
-  :user_name			=> ENV['EMAIL_ADDRESS'],
-  :password				=> ENV['EMAIL_PASSWORD'],
+  :user_name			=> Rails.application.credentials.dig(:email_user_name),
+  :password				=> Rails.application.credentials.dig(:email_password),
   :authentication		=> "plain",
   :enable_starttls_auto	=> true
 }
