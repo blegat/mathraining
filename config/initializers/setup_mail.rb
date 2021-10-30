7b160971692dd2cac46c51061092fa4589fea1e1
Nicolas <nicolasradu16@gmail.com>
2021-10-30 14:49:45 +0200

    Try to use 2-step verification for emails

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port					=> 587,
   :domain				=> "mathraining",
   :user_name			=> Rails.application.credentials.dig(:email_user_name),
-  :password				=> Rails.application.credentials.dig(:email_password),
+  :password				=> Rails.application.credentials.dig(:email_mathraining_key), # email_password cannot be used because of 2-step verification
   :authentication		=> "plain",
   :enable_starttls_auto	=> true
 }
