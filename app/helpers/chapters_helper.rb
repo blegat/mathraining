63735831660c68edec9cd61bd963f1175cbfee38
Nicolas <nicolasradu16@gmail.com>
2021-11-25 14:33:31 +0100

    Fix empty list in sql request (2)

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -51,7 +51,7 @@ module ChaptersHelper
   
   def chapters_completed_request
     if !@signed_in
-      return "('')"
+      return "(0)" # We put 0 because PG does not seem to support empty list
     else
       return "(" + current_user.sk.chapters.select(:id).to_sql + ")"
     end
