25ac10c03a0ebc6b04d0fec35a9657d9ab8aa985
Nicolas <nicolasradu16@gmail.com>
2025-01-04 22:31:11 +0100

    Update defaults for dark user colors

--- app/models/color.rb
+++ app/models/color.rb
@@ -33,15 +33,15 @@ class Color < ActiveRecord::Base
   # DEFAULTS
   
   def self.create_defaults
-    Color.create(pt: 0,    name: "Novice",       color: "#888888", dark_color: "#888888", femininename: "Novice")
-    Color.create(pt: 70,   name: "Débutant",     color: "#08D508", dark_color: "#08D508", femininename: "Débutante")
-    Color.create(pt: 200,  name: "Initié",       color: "#008800", dark_color: "#008800", femininename: "Initiée")
-    Color.create(pt: 400,  name: "Compétent",    color: "#00BBEE", dark_color: "#00BBEE", femininename: "Compétente")
-    Color.create(pt: 750,  name: "Qualifié",     color: "#0033FF", dark_color: "#0033FF", femininename: "Qualifiée")
-    Color.create(pt: 1250, name: "Expérimenté",  color: "#DD77FF", dark_color: "#DD77FF", femininename: "Expérimentée")
-    Color.create(pt: 2000, name: "Chevronné",    color: "#A000A0", dark_color: "#A000A0", femininename: "Chevronnée")
-    Color.create(pt: 3200, name: "Expert",       color: "#FFA000", dark_color: "#FFA000", femininename: "Experte")
-    Color.create(pt: 5000, name: "Maître",       color: "#FF4400", dark_color: "#FF4400", femininename: "Maître")
-    Color.create(pt: 7500, name: "Grand Maître", color: "#CC0000", dark_color: "#CC0000", femininename: "Grand Maître")
+    Color.create(pt: 0,    name: "Novice",       color: "#888888", dark_color: "#A0A0A0", femininename: "Novice")
+    Color.create(pt: 70,   name: "Débutant",     color: "#08D508", dark_color: "#20F720", femininename: "Débutante")
+    Color.create(pt: 200,  name: "Initié",       color: "#008800", dark_color: "#00D300", femininename: "Initiée")
+    Color.create(pt: 400,  name: "Compétent",    color: "#00BBEE", dark_color: "#25D0FF", femininename: "Compétente")
+    Color.create(pt: 750,  name: "Qualifié",     color: "#0033FF", dark_color: "#335CFF", femininename: "Qualifiée")
+    Color.create(pt: 1250, name: "Expérimenté",  color: "#DD77FF", dark_color: "#E492FF", femininename: "Expérimentée")
+    Color.create(pt: 2000, name: "Chevronné",    color: "#A000A0", dark_color: "#BF00E6", femininename: "Chevronnée")
+    Color.create(pt: 3200, name: "Expert",       color: "#FFA000", dark_color: "#FFB333", femininename: "Experte")
+    Color.create(pt: 5000, name: "Maître",       color: "#FF4400", dark_color: "#FF6933", femininename: "Maître")
+    Color.create(pt: 7500, name: "Grand Maître", color: "#CC0000", dark_color: "#FF0A0A", femininename: "Grand Maître")
   end
 end
