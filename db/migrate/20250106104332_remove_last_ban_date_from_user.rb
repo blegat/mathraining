ff8f67512493ccfda7aa24506e5ac17795ecfe96
Nicolas <nicolasradu16@gmail.com>
2025-02-16 12:51:11 +0100

    Fix failing old migration

--- db/migrate/20250106104332_remove_last_ban_date_from_user.rb
+++ db/migrate/20250106104332_remove_last_ban_date_from_user.rb
@@ -1,10 +1,10 @@
 class RemoveLastBanDateFromUser < ActiveRecord::Migration[7.1]
   def change
-    up_only do
-      User.where.not(:last_ban_date => nil).each do |u|
-        Sanction.create(:user_id => u.id, :sanction_type => :ban, :start_time => u.last_ban_date, :duration => 14, :reason => "Ce compte a été temporairement désactivé pour cause de plagiat. Il sera à nouveau accessible le [DATE]. L'équipe des correcteurs bénévoles de Mathraining vous invite à prendre de ce temps libre pour réfléchir à l'intérêt de leur faire corriger des solutions qui ne viennent pas de vous. Notez que la création d'un second compte est formellement interdite et résulterait en une sanction encore plus sévère que celle-ci.")
-      end
-    end
+    #up_only do
+    #  User.where.not(:last_ban_date => nil).each do |u|
+    #    Sanction.create(:user_id => u.id, :sanction_type => :ban, :start_time => u.last_ban_date, :duration => 14, :reason => "Ce compte a été temporairement désactivé pour cause de plagiat. Il sera à nouveau accessible le [DATE]. L'équipe des correcteurs bénévoles de Mathraining vous invite à prendre de ce temps libre pour réfléchir à l'intérêt de leur faire corriger des solutions qui ne viennent pas de vous. Notez que la création d'un second compte est formellement interdite et résulterait en une sanction encore plus sévère que celle-ci.")
+    #  end
+    #end
   
     remove_column :users, :last_ban_date, :datetime
   end
