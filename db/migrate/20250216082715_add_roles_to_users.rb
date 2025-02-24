acb05f380a25850f4e5e515453d6a2e5c3340d56
Nicolas <nicolasradu16@gmail.com>
2025-02-24 11:00:07 +0100

    Fix migration that does not work anymore

--- db/migrate/20250216082715_add_roles_to_users.rb
+++ db/migrate/20250216082715_add_roles_to_users.rb
@@ -2,11 +2,11 @@ class AddRolesToUsers < ActiveRecord::Migration[7.1]
   def change
     add_column :users, :role, :integer, :default => 1
     
-    up_only do
-      User.where(:root => true).update_all(:role => :root)
-      User.where(:admin => true, :root => false).update_all(:role => :administrator)
-      User.where(:active => false).update_all(:role => :deleted)
-    end
+    #up_only do
+    #  User.where(:root => true).update_all(:role => :root)
+    #  User.where(:admin => true, :root => false).update_all(:role => :administrator)
+    #  User.where(:active => false).update_all(:role => :deleted)
+    #end
     
     remove_column :users, :admin, :boolean, :default => false
     remove_column :users, :root, :boolean, :default => false
