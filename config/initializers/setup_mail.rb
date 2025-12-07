661d9f43d158695d59346bb4ac81f127764346f1
Nicolas <nicolasradu16@gmail.com>
2025-12-07 21:50:20 +0100

    Try to change sender for emails

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,20 +1,21 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port	                => 587,
-  :domain               => "gmail.com",
-  :user_name            => Rails.application.credentials.dig(:email_user_name),
-  :password             => Rails.application.credentials.dig(:email_mathraining_key),
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
-
-# Settings to send with no-reply@mathraining.be
+# Settings to send with gmail
 #ActionMailer::Base.smtp_settings = {
-#  :address              => "ssl0.ovh.net",
+#  :address              => "smtp.gmail.com",
 #  :port                 => 587,
-#  :domain               => "mathraining.be",
-#  :user_name            => Rails.application.credentials.dig(:email_user_name), # no-reply@mathraining.be
-#  :password             => Rails.application.credentials.dig(:email_password),
+#  :domain               => "gmail.com",
+#  :user_name            => Rails.application.credentials.dig(:email_user_name), # without @gmail.com
+#  :password             => Rails.application.credentials.dig(:email_mathraining_key),
 #  :authentication       => "plain",
 #  :enable_starttls_auto => true
 #}
+
+# Settings to send with noreply@mathraining.be
+ActionMailer::Base.smtp_settings = {
+  :address              => "pro3.mail.ovh.net",
+  :port                 => 587,
+  :domain               => "mathraining.be",
+  :user_name            => Rails.application.credentials.dig(:email_user_name),
+  :password             => Rails.application.credentials.dig(:email_password),
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
